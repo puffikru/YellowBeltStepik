b0d0a051f7bb3e4a77ba8d56b8e16c217b604fa0
Igor Bulakh <igorbulakh1987@yandex.ru>
2023-06-25 14:02:33 +0300

    Definition functions task is solved

---
+++ sum_reverse_sort/solution.cpp
@@ -0,0 +1,212 @@
+<!DOCTYPE html>
+<!--[if IE 8]><html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!-->
+<html class="no-js"> <!--<![endif]-->
+
+<head>
+  <meta charset="utf-8"/>
+  <link rel="search" type="application/opensearchdescription+xml" href="/static/opensearch.xml?v2" title="Stepik" />
+  <link rel="dns-prefetch" href="//ajax.googleapis.com">
+  <link rel="dns-prefetch" href="//www.google-analytics.com">
+  <link rel="dns-prefetch" href="//stepik.azureedge.net">
+  
+  <meta name="viewport" content="width=device-width, initial-scale=1"/>
+  <meta name="csrf_cookie_name" content="">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+  <meta name="p:domain_verify" content="6e51efa10a4869846bae0b408c5aefcd"/>
+  <title>Stepik &gt; 500</title>
+
+  
+  
+  <link rel="icon" type="image/svg+xml" href="">
+  <link rel="shortcut icon" type="image/x-icon" href="" sizes="16x16 24x24 32x32 48x48 64x64">
+  <link rel="canonical" href="">
+
+  
+    
+  
+
+  <meta property="og:site_name" content="Stepik: online education"/>
+  <meta property="og:url" content=""/>
+  <meta name="twitter:site" content="@stepikorg"/>
+  <meta name="fb:app_id" content="171127739724012"/>
+  <meta name="fb:admins" content="764047960"/>
+
+  
+  
+
+  <!--[if lt IE 9]>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.min.js"></script>
+  <![endif]-->
+  
+  
+  
+  
+  <link rel="stylesheet" href="/static/classic/css/lib.min.css?v=1686814889"/>
+  <link rel="stylesheet" href="/static/frontend-build/vendor.css?v=1686814889">
+  <link rel="stylesheet" href="/static/frontend-build/classic.css?v=1686814889">
+
+  <style type="text/css">
+    body, html {
+      height: 100%;
+      color: #4d4d4d;
+      font-family: Arial, sans-serif;
+      font-weight: 400;
+    }
+
+    .toggle-sidemenu {
+      position: fixed;
+      display: block;
+      width: 42px;
+      height: 42px;
+      top: 20px;
+      left: 20px;
+      cursor: pointer;
+      background-color: #666;
+      z-index: 100;
+      background-image: url(/static/frontend/logo_white.svg);
+    }
+
+    .content-error {
+      color: #231f20;
+      max-width: 1000px;
+      margin: 0 auto;
+      padding: 0 0 50px;
+    }
+
+    .content-error__wrapper {
+      max-width: 650px;
+      padding: 0 10px;
+    }
+
+    .epic-error-img {
+      text-align: center;
+    }
+
+    .epic-error-code {
+      font-size: 100px;
+    }
+
+    .epic-error-text {
+      font-size: 38px;
+      font-weight: bold;
+    }
+
+    .epic-error-text p {
+      font-size: 24px;
+      padding-top: 10px;
+    }
+
+    .epic-link {
+      font-size: 18px;
+      line-height: 1.5;
+      margin-top: 20px;
+    }
+
+    .epic-link a {
+      color: #66cc66;
+      text-decoration: none;
+    }
+
+    .epic-search {
+      border-bottom: 1px solid;
+      padding-top: 30px;
+    }
+
+    .epic-search img {
+      vertical-align: middle;
+    }
+
+    .epic-search input {
+      vertical-align: middle;
+      border: none;
+      background: transparent;
+      font-size: 22px;
+      width: 90%;
+      max-width: 550px;
+      color: #231f20;
+      margin: 0;
+      display: inline-block;
+    }
+
+    .epic-search input:focus {
+      background: transparent;
+    }
+
+    .epic-error-img {
+      display: none;
+    }
+
+    @media (min-width: 768px) {
+      .content-error {
+        padding: 30px 0 30px;
+      }
+
+      .epic-error-img {
+        display: block;
+      }
+
+      .epic-error-img img {
+        width: 200px;
+        height: 200px;
+      }
+    }
+  </style>
+  
+  <style>
+    .epic-error-code, .epic-search {
+      color: #630040;
+    }
+  </style>
+
+
+</head>
+<body>
+
+
+  
+  
+ 
+
+
+  
+  <div class="stepic-application">
+    
+      
+  <div class="content-error">
+    <div class="content-error__wrapper">
+      <div class="epic-error-code">
+        500
+      </div>
+      <div class="epic-error-text">
+        
+  Something went wrong :(
+  <p>Either we have troubles on server and have already recorded them or Stepik is being updated right now</p>
+
+      </div>
+      <div class="epic-link">
+        
+          Check <a href="/">Home page</a>
+          
+          or <a href="http://support.stepik.org">contact us</a>
+        
+      </div>
+    </div>
+    <div class="epic-error-img">
+      <img src="/static/frontend/error_500.svg">
+    </div>
+  </div>
+
+    
+  </div>
+
+  
+  
+
+
+
+
+
+
+</body>
+</html>
